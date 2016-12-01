d5b337dff1d3746a99ad7d244e285728dbcc0900
Paul Milne <hallhill@gmail.com>
2016-12-01 14:16:32 +0000

    wrote test that bank has accounts.

--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -1,3 +1,14 @@
 var Bank = require('../bank');
 var assert = require('assert');
 
+var bank;
+
+describe('Bank', function(){
+     beforeEach(function(){
+          bank = new Bank();
+     });
+
+     it('has a collection of accounts which starts empty', function(){
+          assert.equal(0, bank.accounts.length);
+     })
+})
